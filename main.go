49ebe3ae393d60750af70d52e7e3823043081331
potpot <chaukinyi1029@gmail.com>
2026-04-20 23:05:34 +0800

    feat: pokedex REPL

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+
+		}
+		userInput := scanner.Text()
+		cleanUserInput := cleanInput(userInput)
+		fmt.Printf("Your command was: %s\n", cleanUserInput[0])
+	}
 }
